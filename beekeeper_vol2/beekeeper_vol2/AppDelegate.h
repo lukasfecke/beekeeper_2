4d62330f7c6d6e0c846ec7f7c04cffeb92a13b21
Lukas Fecke <fecke.lukas@gmail.com>
2017-02-09 12:55:18 +0100

    initial commit

---
+++ beekeeper_vol2/beekeeper_vol2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beekeeper_vol2
+//
+//  Created by Lukáš Fečke on 09/02/2017.
+//  Copyright © 2017 Lukáš Fečke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beekeeper_vol2/beekeeper_vol2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beekeeper_vol2
+//
+//  Created by Lukáš Fečke on 09/02/2017.
+//  Copyright © 2017 Lukáš Fečke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
